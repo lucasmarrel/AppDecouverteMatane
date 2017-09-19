5cca4666d2e94a5d935a7f356bfa4051e75d4d5f
lucasmarrel <marrel.lucas@gmail.com>
2017-09-19 10:46:53 -0400

    creation projet

---
+++ app/src/main/java/evenements/informatique/cqmatane/qc/ca/appdecouvertematane/VuePrincipale.java
@@ -0,0 +1,13 @@
+package evenements.informatique.cqmatane.qc.ca.appdecouvertematane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VuePrincipale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_principale);
+    }
+}

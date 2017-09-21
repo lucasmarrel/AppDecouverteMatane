61b4dc7d73d06e91a7a9a3247963a98982eb022e
Stocky93420 <navpreet.mail@gmail.com>
2017-09-21 16:51:35 -0400

    Ajout modele Emplacement

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/appdecouvertematane/modele/Emplacement.java
@@ -0,0 +1,69 @@
+package ca.qc.cgmatane.informatique.appdecouvertematane.modele;
+
+/**
+ * Created by 1732986 on 2017-09-21.
+ */
+
+public class Emplacement
+{
+    private int id;
+    private String nom;
+    private double latitude;
+    private double longitude;
+
+    public Emplacement(int id, String nom, double latitude, double longitude)
+    {
+        this.id = id;
+        this.nom = nom;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Emplacement()
+    {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Emplacement{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
+

aad094299dfabdce3b68e9d490f5b24e7822807f
Stocky93420 <navpreet.mail@gmail.com>
2017-09-21 16:36:23 -0400

    Ajout packages modele/vue/donnees

--- app/src/main/java/ca/qc/cgmatane/informatique/appdecouvertematane/vue/VuePrincipale.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/appdecouvertematane/vue/VuePrincipale.java
@@ -1,7 +1,9 @@
-package ca.qc.cgmatane.informatique.appdecouvertematane;
+package ca.qc.cgmatane.informatique.appdecouvertematane.vue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ca.qc.cgmatane.informatique.appdecouvertematane.R;
 
 public class VuePrincipale extends AppCompatActivity {
 

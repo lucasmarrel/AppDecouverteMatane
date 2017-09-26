91c87b7599bc5380101c96130123877abb40f16e
lucasmarrel <marrel.lucas@gmail.com>
2017-09-26 12:07:02 -0400

    Ajout code SQL insertion tables base de données

--- app/src/main/java/ca/qc/cgmatane/informatique/appdecouvertematane/donnees/BaseDeDonnees.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/appdecouvertematane/donnees/BaseDeDonnees.java
@@ -25,23 +25,49 @@ public class BaseDeDonnees extends SQLiteOpenHelper {
     }
 
     public BaseDeDonnees(Context contexte) {
-        super(contexte, "evenement", null, 1);
+        super(contexte, "appMatane", null, 1);
 
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        String CREATE_TABLE = "create table evenement(id_evenement INTEGER PRIMARY KEY, titre TEXT, lieu TEXT, date DATETIME, description TEXT)";
-//        db.execSQL(CREATE_TABLE);
+
+//        String TABLE_EMPLACEMENT = "create table emplacement(id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, latitude REAL, longitude REAL)";
+//        String TABLE_UTILISATEUR = "create table utilisateur(id INTEGER PRIMARY KEY AUTOINCREMENT, prenom TEXT, nom TEXT, nomUtilisateur TEXT, mail TEXT, motDePasse TEXT)";
+//        String TABLE_PHOTO = "create table photo(id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT)";
+//        String TABLE_QRCODE = "create table qrCode(id TEXT PRIMARY KEY)";
+
+//        db.execSQL(TABLE_EMPLACEMENT);
+//        db.execSQL(TABLE_UTILISATEUR);
+//        db.execSQL(TABLE_PHOTO);
+//        db.execSQL(TABLE_QRCODE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
-//        String DETRUIRE_TABLE = "drop table evenement";
-//        db.execSQL(DETRUIRE_TABLE);
-//        String CREATE_TABLE = "create table evenement(id_evenement INTEGER PRIMARY KEY AUTOINCREMENT, titre TEXT, lieu TEXT, date DATETIME, description TEXT)";
-//        db.execSQL(CREATE_TABLE);
+        
+//        String DETRUIRE_TABLE_EMPLACEMENT = "drop table evenement";
+//        db.execSQL(DETRUIRE_TABLE_EMPLACEMENT);
+
+//        String DETRUIRE_TABLE_UTILISATEUR = "drop table utilisateur";
+//        db.execSQL(DETRUIRE_TABLE_UTILISATEUR);
+
+//        String DETRUIRE_TABLE_PHOTO = "drop table photo";
+//        db.execSQL(DETRUIRE_TABLE_PHOTO);
+
+//        String DETRUIRE_TABLE_QRCODE = "drop table qrCode";
+//        db.execSQL(DETRUIRE_TABLE_QRCODE);
+
+//        String TABLE_EMPLACEMENT = "create table emplacement(id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, latitude REAL, longitude REAL)";
+//        String TABLE_UTILISATEUR = "create table utilisateur(id INTEGER PRIMARY KEY AUTOINCREMENT, prenom TEXT, nom TEXT, nomUtilisateur TEXT, mail TEXT, motDePasse TEXT)";
+//        String TABLE_PHOTO = "create table photo(id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT)";
+//        String TABLE_QrCode = "create table qrCode(id TEXT PRIMARY KEY)";
+
+//        db.execSQL(TABLE_EMPLACEMENT);
+//        db.execSQL(TABLE_UTILISATEUR);
+//        db.execSQL(TABLE_PHOTO);
+//        db.execSQL(TABLE_QrCode);
     }
 
     @Override

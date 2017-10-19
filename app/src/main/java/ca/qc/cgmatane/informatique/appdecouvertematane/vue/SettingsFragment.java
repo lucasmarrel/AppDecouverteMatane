9c8dced2df9e99195479cb9ed9831eb5e8d6d61b
Kelyan_CHAUFFOURIER <kelyan.chauffourier@gmail.com>
2017-10-19 18:13:21 -0400

    Ajout prise en charge XML + fragment

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/appdecouvertematane/vue/SettingsFragment.java
@@ -0,0 +1,30 @@
+package ca.qc.cgmatane.informatique.appdecouvertematane.vue;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.qc.cgmatane.informatique.appdecouvertematane.R;
+
+/**
+ * Created by Kelyan Chauffourier on 2017-10-19.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(getResources().getColor(android.R.color.white));
+        return view;
+    }
+}
